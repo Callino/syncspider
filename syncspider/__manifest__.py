8ad186616d6656b97557318094807b88c9ed4890
gbaumgartner <gbaumgartner@callino.at>
2020-04-01 15:00:08 +0200

    [IMP] updates version nr

--- syncspider/__manifest__.py
+++ syncspider/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Tools',
-    'version': '12.0.1.0',
+    'version': '13.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mail'],
