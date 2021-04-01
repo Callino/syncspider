3db1811d78124d070f958da6666e61552547db40
Wolfgang Pichler <wpichler@callino.at>
2021-04-01 10:52:26 +0200

    [ADD] Added methods to add and delete attributes

--- syncspider/models/product_template.py
+++ syncspider/models/product_template.py
@@ -40,21 +40,32 @@ class ProductTemplate(models.Model):
         for json_template in jsondata:
             template = self.browse(json_template['product_tmpl_id'])
             templates += template
+            used_attribute_ids = []
             for json_att in json_template['attributes']:
-                _logger.info("Got Attributes: %s", json_att)
+                used_attribute_ids.append(json_att['attribute_id'])
                 att_line = template.attribute_line_ids.filtered(lambda line: line.attribute_id.id == json_att['attribute_id'])
                 if not att_line:
                     # Seems to be a new attribute - so create a new att_line for it
-                    pass
-                else:
+                    values = []
                     for value in json_att['values']:
-                        if value['active']:
-                            t_value = att_line.value_ids.filtered(lambda v: v.id == value['value_id'])
-                            if not t_value:
-                                att_line.value_ids += self.env['product.attribute.value'].browse(value['value_id'])
-                        else:
-                            t_value = att_line.value_ids.filtered(lambda v: v.id == value['value_id'])
-                            if t_value:
-                                att_line.value_ids -= self.env['product.attribute.value'].browse(value['value_id'])
+                        if not value['active']:
+                            continue
+                        values.append((4, value['value_id']))
+                    template.attribute_line_ids = [(0, 0, {
+                        'attribute_id': json_att['attribute_id'],
+                        'value_ids': values,
+                    })]
+                    continue
+                for value in json_att['values']:
+                    if value['active']:
+                        t_value = att_line.value_ids.filtered(lambda v: v.id == value['value_id'])
+                        if not t_value:
+                            att_line.value_ids += self.env['product.attribute.value'].browse(value['value_id'])
+                    else:
+                        t_value = att_line.value_ids.filtered(lambda v: v.id == value['value_id'])
+                        if t_value:
+                            att_line.value_ids -= self.env['product.attribute.value'].browse(value['value_id'])
+            for att_line in template.attribute_line_ids.filtered(lambda al: al.attribute_id.id not in used_attribute_ids):
+                att_line.unlink()
         templates.create_variant_ids()
         return templates.get_template_json()
