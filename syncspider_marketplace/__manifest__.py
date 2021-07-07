41a2618a0cd2859393bd9917e23f748661322f37
gbaumgartner <gbaumgartner@callino.at>
2021-07-07 11:20:06 +0200

    [add] adds syncspider_marketplace

---
+++ syncspider_marketplace/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Syncspider - Marketplace",
+
+    'summary': """
+        """,
+
+    'description': """
+        Syncspider - Marketplace
+    """,
+
+    'author': "Callino",
+    'website': "http://www.callino.at",
+    'category': 'Misc',
+    'version': '13.0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['syncspider', 'product'],
+
+    # always loaded
+    'data': [
+        'views/product.xml'
+    ],
+    'qweb': [
+    ],
+
+}
