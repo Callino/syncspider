4d309429bc5a6a107129a49c3c9be4863bdd88e9
gbaumgartner <gbaumgartner@callino.at>
2023-03-23 10:39:00 +0100

    [IMP] improves set_qty_and_mark_done

--- syncspider/models/stock_picking.py
+++ syncspider/models/stock_picking.py
@@ -11,10 +11,14 @@ class StockPicking(models.Model):
 
     def set_qty_and_mark_done(self):
         for picking in self:
+            if picking.state == 'confirmed':
+                picking.action_assign()
             for line in picking.move_ids_without_package:
                 try:
                     line.write({'quantity_done': line.product_uom_qty, 'state': 'assigned'})
                 except Exception as e:
-                    _logger.exception("Cannot write line qty at this point.")
+                    for ml in picking.move_line_ids_without_package:
+                        ml.write({'qty_done': ml.product_uom_qty, 'state': 'assigned'})
+                    # _logger.exception("Cannot write line qty at this point.")
             picking.button_validate()
         return True
