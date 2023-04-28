c663a2a00eaa5de697292481f9a9a48b81c70622
gbaumgartner <gbaumgartner@callino.at>
2023-04-28 11:07:44 +0200

    [IMP] adds wrapper method for _create_invoices

---
+++ syncspider/models/sale_order.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from collections import defaultdict
+from odoo.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def create_invoices(self):
+        return self._create_invoices()
