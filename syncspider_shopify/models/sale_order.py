a7f219461fc20b222bfff0022183e9119783dd57
gbaumgartner <gbaumgartner@callino.at>
2025-09-16 11:36:43 +0200

    [IMP] set flag to default True for auto_downpayment

--- syncspider_shopify/models/sale_order.py
+++ syncspider_shopify/models/sale_order.py
@@ -43,7 +43,7 @@ class SaleOrder(models.Model):
         ('Voided', _('Voided')),
         ('Unpaid', _('Unpaid')),
     ], string="Payment Status", readonly=True)
-    auto_downpayment = fields.Boolean(string="Automatische Anzahlung", default=False)
+    auto_downpayment = fields.Boolean(string="Automatische Anzahlung", default=True)
     shopify_delivery_method = fields.Char(string="Liefermethode", readonly=1)
     shopify_delivery_amount = fields.Float(string="Lieferbetrag", readonly=1)
     shopify_global_discount_amount = fields.Float(string="Monetärer Discount auf Auftrag", tracking=True)
